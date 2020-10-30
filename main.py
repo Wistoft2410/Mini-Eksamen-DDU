4f5263a587906800ddcb784cb70a2120b6bc45c2
fijibottle <gulfurs@gmail.com>
2020-10-30 13:53:25 +0100

    #4 elev login, starttest, test
    sat html filer op, og update stylesheet

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=('GET',))
 def home():
-    return render_template('startside.html')
+    return render_template('hovedmenu.html')
 
 
 @app.route('/layout', methods=('GET',))
@@ -14,6 +14,14 @@ def layout():
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     return render_template('login.html')
+
+@app.route('/startside', methods=('GET', 'POST'))
+def start():
+    return render_template('startside.html')
+
+@app.route('/test', methods=('GET', 'POST'))
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run()
