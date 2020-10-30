be6442c5c89b22620b9d50f82817121bca6a71e5
Marcus <mjagdthomsen@gmail.com>
2020-10-30 12:26:48 +0100

    #4
    inputfelt

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=('GET',))
 def home():
-    return render_template('layout.html')
+    return render_template('startside.html')
 
 if __name__ == '__main__':
     app.run()
