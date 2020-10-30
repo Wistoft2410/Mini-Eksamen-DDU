88b3433000297657bb4bd5e5bc7b78ee8394aa99
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2020-10-30 12:40:09 +0100

    tilføjet noget nyt

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ def home():
     return render_template('startside.html')
 
 
-@app.route('/layout', methods=('GET', 'POST'))
+@app.route('/layout', methods=('GET',))
 def layout():
     return render_template('layout.html')
 
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
