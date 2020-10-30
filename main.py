64195341abe20ed673ef8365fa1ea1340a700435
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2020-10-30 12:27:27 +0100

    fikset noget

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('startside.html')
 
+
+@app.route('/layout', methods=('GET', 'POST'))
+def layout():
+    return render_template('layout.html')
+
+
 if __name__ == '__main__':
     app.run()
